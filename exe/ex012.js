fe8fcc9333f53a1e6a2c2bfd35075a839399ef21
Pablo Martins <goncalvesmartins5454@gmail.com>
2025-09-23 12:08:12 -0300

    exercicios

---
+++ exe/ex012.js
@@ -0,0 +1,12 @@
+function PI () {
+    return 3.14;
+}
+PI()
+console.log(PI());
+
+let raio = 2;
+function raioCircunferencia(raio) {
+    return raio / (2 * PI());
+}
+raioCircunferencia(raio)
+console.log(raioCircunferencia(10));
---
+++ exe/ex013.js
@@ -0,0 +1,12 @@
+const nomeCompletoAnonima = function (nome,sobrenome) {
+    console.log(`${nome} ${sobrenome}`);
+}
+
+nomeCompletoAnonima('Pablo', 'Martins');
+
+
+const nomeCompleto = (nome,sobrenome) => {
+    console.log(`${nome} ${sobrenome}`);
+}
+
+nomeCompleto('Pablo', 'Martins');
